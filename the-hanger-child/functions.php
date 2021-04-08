cae721620d2418ac58154315498c607d99f2948e
Serhii <smolanovsergey@gmail.com>
2021-04-08 21:04:27 +0300

    Initial commit

---
+++ the-hanger-child/functions.php
@@ -0,0 +1,15 @@
+<?php //Start building your awesome child theme functions
+
+add_action( 'wp_enqueue_scripts', 'thehanger_enqueue_styles', 99 );
+function thehanger_enqueue_styles() {
+
+    // enqueue parent styles
+    wp_enqueue_style( 'thehanger-styles', get_template_directory_uri() . '/css/styles.css' );
+
+    // enqueue child styles
+    wp_enqueue_style( 'thehanger-child-styles',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'thehanger-styles' ),
+        wp_get_theme()->get('Version')
+    );
+}
